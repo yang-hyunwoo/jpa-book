3ff3bf618854bfa2e64c4bea7f62e9c395c553ac
cohouseol <cohouseol@gmail.com>
2022-12-14 02:57:34 +0900

    ㄱ
    ㄱ

---
+++ info-run/JavaTPC/src/basic/TPC31.java
@@ -0,0 +1,13 @@
+package basic;
+
+import kr.tpc.Inflearn;
+
+public class TPC31 {
+
+    public static void main(String[] args) {
+
+        Inflearn.java();
+        Inflearn.tpc();
+
+    }
+}
---
+++ info-run/JavaTPC/src/kr/tpc/Inflearn.java
@@ -0,0 +1,18 @@
+package kr.tpc;
+
+public class Inflearn {
+
+    private Inflearn() {
+
+    }
+    //인스턴스 메서드
+    public static void tpc() {
+        System.out.println("TPC");
+    }
+
+    //클래스 메서드
+    public static void java() {
+        System.out.println("자바");
+    }
+
+}
