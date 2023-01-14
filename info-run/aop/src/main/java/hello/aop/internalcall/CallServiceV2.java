4e054794f5a9b0f9a396ef4d8243908b6cbad984
cohouseol <cohouseol@gmail.com>
2023-01-14 20:06:20 +0900

    공부

---
+++ info-run/aop/src/main/java/hello/aop/internalcall/CallServiceV2.java
@@ -0,0 +1,31 @@
+package hello.aop.internalcall;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class CallServiceV2 {
+
+//    private final ApplicationContext applicationContext;
+
+    private final ObjectProvider<CallServiceV2> callServiceV2Provider;
+
+    public CallServiceV2(ObjectProvider<CallServiceV2> callServiceV2Provider) {
+        this.callServiceV2Provider = callServiceV2Provider;
+    }
+
+    public void external() {
+        log.info("call external");
+        CallServiceV2 callServiceV2 = callServiceV2Provider.getObject();
+//        CallServiceV2 callServiceV2 = applicationContext.getBean(CallServiceV2.class);
+        callServiceV2.internal(); //내부 메서드 호출 (this.internal())
+    }
+
+    public void internal() {
+        log.info("call internal");
+    }
+}
